4d3659e58c81b862ab5a944c7a79c8379eac22dd
Mike Curtis <mikecurtis1@gmail.com>
2015-12-15 14:50:46 -0500

    Update generic.js

--- XSL/EAD/generic.js
+++ XSL/EAD/generic.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.toggle').nextAll().toggle();
     $('.toggle').click(function () {     
-        $(this).nextAll().toggle("slow");
+        $(this).nextAll().toggle(1200);
         var inner = $(this).text().charCodeAt(0);
         if (inner == '9660') {
             $(this).html('&#9650;');
